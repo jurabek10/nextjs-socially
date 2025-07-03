71b381b6d80a9aa9e6d4af0dd252c349ed76d209
jurabek10 <samiev.jurabek@bk.ru>
2025-07-03 18:52:40 +0900

    feat: develop Navabr components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,8 @@ import Module from "module";
 
 export default function Home() {
   return (
-    <div>
-      <SignedOut>
-        <SignUpButton>
-          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
+    <div className="m-4">
+      <h1>home page comtent</h1>
     </div>
   );
 }

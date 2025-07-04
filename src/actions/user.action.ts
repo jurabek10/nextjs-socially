26c93820c812ed7a05e932e0b72ef7c9e9016d6b
jurabek10 <samiev.jurabek@bk.ru>
2025-07-04 22:57:23 +0900

    feat: develop like, delete post and comment post logic

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -54,7 +54,7 @@ export async function getUserByClerkId(clerkId: string) {
 
 export async function getDbUserId() {
   const { userId: clerkId } = await auth();
-  if (!clerkId) throw new Error("Unauthorized");
+  if (!clerkId) return null;
 
   const user = await getUserByClerkId(clerkId);
   if (!user) throw new Error("User not found");
@@ -65,6 +65,8 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
   try {
     const userId = await getDbUserId();
+    if (!userId) return [];
+
     const randoUsers = await prisma.user.findMany({
       where: {
         AND: [

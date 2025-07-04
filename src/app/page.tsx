3368389433967b376e905381bbddfad4a9530673
jurabek10 <samiev.jurabek@bk.ru>
2025-07-04 17:13:23 +0900

    feat: develop createPost component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CreatePost from "@/components/CreatePost";
 import ModeToggle from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,12 +8,25 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Module from "module";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>home page comtent</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">
+        {user ? <CreatePost /> : null}
+
+        {/* <div className="space-y-6">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId} />
+          ))}
+        </div> */}
+      </div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        WhoToFollow
+      </div>
     </div>
   );
 }
